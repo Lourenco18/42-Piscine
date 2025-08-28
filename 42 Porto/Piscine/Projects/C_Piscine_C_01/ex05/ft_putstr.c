eecb146f1bca4d279e62b5c98a57cd2fdd81fc06
Daniel Lourenço <153820691+Lourenco18@users.noreply.github.com>
2025-08-28 17:22:44 +0100

    Add files via upload

--- 42 Porto/Piscine/Projects/C_Piscine_C_01/ex05/ft_putstr.c
+++ 42 Porto/Piscine/Projects/C_Piscine_C_01/ex05/ft_putstr.c
@@ -1,18 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasantos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 16:39:51 by dasantos          #+#    #+#             */
+/*   Updated: 2025/08/28 17:22:12 by dasantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-void	ft_putstr(char *str)
+
+void	ft_putstr(char *str )
+
 {
-	while (*str)
+	int 	i;
+	
+	i = 0;
+	while (str[i])
 	{
-		write(1, str, 1);
-		str++;
+		char	c;
+
+		c = *str[i];
+		write(1, &c, 1);
+		i++;
 	}
 }
 
-/*
+
 int main(void)
 {
-    ft_putstr("Hello, 42 Porto!\n");
+    ft_putstr("frase para exerccio05");
     return 0;
 }
-*/
 
