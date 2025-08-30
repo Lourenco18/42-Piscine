ee1564acdab5f6ea6501c5ebecc78b6b3240ed15
Daniel Lourenço <153820691+Lourenco18@users.noreply.github.com>
2025-08-30 12:26:49 +0100

    ex11

--- 42 Porto/Piscine/Projects/C_Piscine_C_02/ex11/ft_putstr_non_printable.c
+++ 42 Porto/Piscine/Projects/C_Piscine_C_02/ex11/ft_putstr_non_printable.c
@@ -1,12 +1,33 @@
-int ft_putstr_non_printable(char *str){
+#include <unistd.h>
+
+void ft_putstr_non_printable(char *str)
+{
     int i = 0;
+    char hex_digits[] = "0123456789abcdef";
 
     while (str[i] != '\0')
     {
-        if (str[i] < 32 || str[i] > 126)
-            return 0;
+        if (str[i] < 32 || str[i] == 127)
+        {
+            char c = (unsigned char)str[i];
+            write(1, "\\", 1);
+            write(1, &hex_digits[c / 16], 1); 
+            write(1, &hex_digits[c % 16], 1); 
+        }
+        else
+        {
+            write(1, &str[i], 1);
+        }
         i++;
     }
-    return 1;
 }
+/*
+int main(void)
+{
+    char str[] = "Ola\nesta bem?\tFim\x7f";
 
+    ft_putstr_non_printable(str);
+    write(1, "\n", 1);
+    return 0;
+}
+    */
